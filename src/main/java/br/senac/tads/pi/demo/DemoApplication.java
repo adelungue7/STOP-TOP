be406caa2d495dc019cdccdb5c6814a739d04549
RAFAEL ADELUNGUE DA SILVA <rafael.asilva108@senacsp.edu.br>
2026-04-15 21:25:14 -0300

    criacao do projeto

---
+++ src/main/java/br/senac/tads/pi/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 9b61a2f68d0e8e7d820f5023a4c51a6a7cdf75fbde85272b54fe90f2950bb20b
+package br.senac.tads.pi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/tads/pi/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 9b61a2f68d0e8e7d820f5023a4c51a6a7cdf75fbde85272b54fe90f2950bb20b
+package br.senac.tads.pi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
